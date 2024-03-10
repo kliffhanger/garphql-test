5e49c6deda1b77022ed3e0810ff9418b1a91f664
kliffhanger <30635176+kliffhanger@users.noreply.github.com>
2024-03-11 01:54:02 +0530

    apollo server setup and basic ql setup

---
+++ index.js
@@ -0,0 +1,40 @@
+import { ApolloServer } from "@apollo/server";
+import {startStandaloneServer} from '@apollo/server/standalone'
+
+import { typeDefs } from "./schema";
+
+import db from './_db.js'
+
+const resolvers = {
+    Query:{
+        authors(){
+            return db.authors
+        },
+        categories(){
+            return db.categories
+        },
+        articles(){
+            return db.articles
+        },
+        author(_, args){
+            return db.authors.find((author)=> author.id === args.id)
+        },
+        category(_, args){
+            return db.categories.find((item)=> item.id === args.id)
+        },
+        article(_, args){
+            return db.articles.find((item)=> item.id === args.id)
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+const {url} = await startStandaloneServer(server,{
+    listen: {port: 4000}
+})
+
+console.log('server is running on port', 4000)
